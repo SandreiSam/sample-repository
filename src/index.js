db873e82c5213b85cc27ddacb42ca11121853cfb
SandreiSam <152827248+SandreiSam@users.noreply.github.com>
2025-05-19 17:29:19 +0800

    ITMC313

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { LandingApplicant } from "./LandingApplicant";
+import "./Landing.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<LandingApplicant />);
